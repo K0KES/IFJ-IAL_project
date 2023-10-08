14cc7e0e7b7fe6cbb196008c191ccfee88c6c13f
Petr <piter.stancl@email.cz>
2023-10-08 18:50:17 +0200

    Scanner first code

---
+++ src/scanner.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <malloc.h>
+#include "scanner.h"
+
+
+FILE *source;
+
+void setSourceFile(FILE *f) {
+    source = f;
+}
+
+int getToken(token *token) {
+    char c;
+    char lastChar;
+    int charNumber = 0;
+    int lineNumber = 1;
+    enum state state = S_START;
+    printf("\"");
+    while (c != EOF) {
+        c = getc(source);
+        printf("%c", c);
+        switch (state) {
+            case S_START:
+                switch (c) {
+                    case '\t':
+                    case ' ':
+                        state = S_START;
+                        break;
+                    case '\n':
+                    case '\r':
+                        lineNumber++;
+                        charNumber = 0;
+                        break;
+                    //STATES chars
+                    case '+':
+                        strAddChar(token->value, c);
+                        state = S_INCREMENT;
+                        break;
+                    case '-':
+                        strAddChar(token->value, c);
+                        state = S_DECREMENT;
+                        break;
+                    case '<':
+                        strAddChar(token->value, c);
+                        state = S_LESS_EQUAL;
+                        break;
+                    case '>':
+                        strAddChar(token->value, c);
+                        state = S_GREATER_EQUAL;
+                        break;
+                    case '=':
+                        strAddChar(token->value, c);
+                        state = S_EQUAL;
+                        break;
+                    case '?':
+                        strAddChar(token->value, c);
+                        state = S_NIL_OP;
+                        break;
+                    case '!':
+                        strAddChar(token->value, c);
+                        state = S_NOT_EQUAL;
+                        break;
+                    case '_':
+                        strAddChar(token->value, c);
+                        state = S_UNDERSCORE_IDENTIFIER;
+                        break;
+
+                    //EASY chars
+                    case '*':
+                        token->tokenType = T_MULTIPLICATION;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    case '(':
+                        token->tokenType = T_LEFT_BRACKET;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    case ')':
+                        token->tokenType = T_RIGHT_BRACKET;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    case '{':
+                        token->tokenType = T_LEFT_CURLY_BRACKET;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    case '}':
+                        token->tokenType = T_RIGHT_CURLY_BRACKET;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    case ':':
+                        token->tokenType = T_COLON;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    case ';':
+                        token->tokenType = T_SEMICOLON;
+                        strAddChar(token->value, c);
+                        return 1;
+                        break;
+                    default:
+                        if (isalpha(c)) { 
+                            strAddChar(token->value, c);  
+                            state = S_IDENTIFIER_KW;
+                        }
+
+                        else if(isalnum(c)) {
+                            strAddChar(token->value, c);  
+                            state = S_INT;
+                        }
+                        break;
+                }
+                charNumber++;
+                lastChar = c;
+                break;
+            //STATES
+            case S_INCREMENT:
+                if (c == '+') { 
+                    token->tokenType = T_INCREMENT;
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_PLUS;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+
+             case S_DECREMENT:
+                if (c == '-') { 
+                    token->tokenType = T_DECREMENT;
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_MINUS;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+
+             case S_LESS_EQUAL:
+                if (c == '=') { 
+                    token->tokenType = T_LESS_EQUAL;
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_LESS;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+
+             case S_GREATER_EQUAL:
+                if (c == '=') { 
+                    token->tokenType = T_GREATER_EQUAL;
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_GREATER;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+            
+            case S_EQUAL:
+                if (c == '=') { 
+                    token->tokenType = T_EQUAL; 
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_ASSIGNMENT;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+
+            case S_NIL_OP:
+                if (c == '?') { 
+                    token->tokenType = T_NIL_OP; 
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_NULLABLE;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+            case S_NOT_EQUAL:
+                if (c == '=') { 
+                    token->tokenType = T_NOT_EQUAL; 
+                    strAddChar(token->value, c);
+                    return 1;
+                }
+                else {
+                    token->tokenType = T_NOT;
+                    ungetc(c, source);
+                    return 1;
+                }
+                break;
+            case S_UNDERSCORE_IDENTIFIER:
+                switch (c) {
+                    //it is _
+                    case '\r':
+                    case '\t':
+                    case ' ':
+                    case '\n':
+                        token->tokenType = KW_UNDERSCORE;
+                        ungetc(c, source);
+                        return 1;
+                        break;
+                    //it is identifier
+                    default:
+                        if (isalnum(c) || c == '_') {
+                            state = S_IDENTIFIER_KW;
+                            strAddChar(token->value, c);
+                        }
+                        break;
+                }
+                break;
+            case S_IDENTIFIER_KW:
+                switch (c) {
+                //KEYWORD
+                case '\r':
+                case '\t':
+                case ' ':
+                case '\n':
+                    if (strCmpConstStr(token->value, "Double") == 0) {
+                        token->tokenType = KW_DOUBLE;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "else") == 0) {
+                        token->tokenType = KW_ELSE;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "func") == 0) {
+                        token->tokenType = KW_FUNC;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "if") == 0) {
+                        token->tokenType = KW_FUNC;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "Int") == 0) {
+                        token->tokenType = KW_IF;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "let") == 0) {
+                        token->tokenType = KW_INT;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "nil") == 0) {
+                        token->tokenType = KW_NIL;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "return") == 0) {
+                        token->tokenType = KW_RETURN;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "String") == 0) {
+                        token->tokenType = KW_STRING;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "var") == 0) {
+                        token->tokenType = KW_VAR;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    if (strCmpConstStr(token->value, "while") == 0) {
+                        token->tokenType = KW_WHILE;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    else {
+                        token->tokenType = T_IDENTIFIER;
+                        ungetc(c, source);
+                        return 1;
+                    }       
+                    break;
+                //IDENTIFIER
+                default:
+                    if (isalnum(c) || c == '_') {
+                        strAddChar(token->value, c);
+                    }
+                    else {
+                        token->tokenType = T_IDENTIFIER;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    break;
+                }
+                break;
+            case S_INT:
+                switch (c) {
+                case '.':
+                    state = S_DOUBLE;
+                    strAddChar(token->value, c);
+                    break;
+                
+                default:
+                    if (!isalpha(c) && isalnum(c)) {  
+                        strAddChar(token->value, c);
+                    }
+                    else {
+                        token->tokenType = T_INT;
+                        ungetc(c, source);
+                        return 1;
+                    }
+                    break;
+                }
+                break;
+        }
+
+
+    }
+    return 2;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    token *token_e = (token*)malloc(sizeof(token));
+    strInit(token_e->value);
+
+    FILE *f;
+    if (argc == 1) {
+        printf("Neni zadan vstupni soubor\n");
+        return 1;
+    }
+    if ((f = fopen(argv[1], "r")) == NULL) {
+        printf("Soubor se nepodarilo otevrit\n");
+        return 2;
+    }   
+    setSourceFile(f);
+    int tokenReturnValue = getToken(token_e);   
+    while (tokenReturnValue != 2) {
+        printf("\"");
+        printf("\ntoken type: %d, value: ", token_e->tokenType);
+        strPrint(token_e->value);
+        printf("\n\n");
+        strClear(token_e->value);
+        tokenReturnValue = getToken(token_e);
+    };
+
+}
---
+++ src/scanner.h
@@ -0,0 +1,89 @@
+#include "str.h"
+
+typedef struct {
+    int lineNumber;
+    int charNumber;
+} positionInfo;
+
+enum tokenType {
+    T_WHITESPACE,
+    T_EOF,
+    T_START,
+    T_IDENTIFIER,
+
+    //BRACKETS
+    T_LEFT_BRACKET,
+    T_RIGHT_BRACKET,
+    T_LEFT_CURLY_BRACKET,
+    T_RIGHT_CURLY_BRACKET,
+
+    //OPERATORS
+    T_PLUS,
+    T_MINUS,
+    T_MULTIPLICATION,
+    T_DIVISION,
+    T_LESS,
+    T_LESS_EQUAL,
+    T_GREATER,
+    T_GREATER_EQUAL,
+    T_EQUAL,
+    T_NOT_EQUAL,
+    T_NIL_OP,
+
+    //VARIABLES
+    T_ASSIGNMENT,
+    T_INCREMENT,
+    T_DECREMENT,
+    T_VAR_MUL_VAR,
+    T_VAR_DIV_VAR,
+    T_NOT,
+
+    //DATA TYPES
+    T_INT,
+    T_DOUBLE,
+    T_STRING,
+    T_NULLABLE,
+
+    //OTHERS
+    T_SEMICOLON,
+    T_COLON,
+
+    //KEYWORDS
+    KW_DOUBLE,
+    KW_FUNC,
+    KW_RETURN,
+    KW_INT,
+    KW_STRING,
+    KW_NIL,
+    KW_ELSE,
+    KW_IF,
+    KW_WHILE,
+    KW_VAR,
+    KW_LET,
+    KW_UNDERSCORE
+};
+
+
+enum state {
+    S_START,
+    S_IDENTIFIER_KW,
+    S_INT,
+    S_DOUBLE,
+    S_UNDERSCORE_IDENTIFIER,
+    S_LESS_EQUAL,
+    S_GREATER_EQUAL,
+    S_EQUAL,
+    S_NOT_EQUAL,
+    S_INCREMENT,
+    S_DECREMENT,
+    S_VAR_MUL_VAR,
+    S_VAR_DIV_VAR,
+    S_NIL_OP
+};
+
+typedef struct {
+    enum tokenType tokenType;
+    string *value;
+    positionInfo *position;
+} token;
+
