0b4596e0f2de7be0412669fb203cd5ccc88f6f33
Prokes Lukas <lukas.prokes03@gmail.com>
2023-11-16 01:11:07 +0100

    Comments for parser functions added

--- src/parser.h
+++ src/parser.h
@@ -26,107 +26,132 @@ int parse(token *activeToken, symtable *symTablePtr);
 
 /// @brief Function gets next token and saves it in activeToken
 /// @param activeToken pointer to token
-/// @return returns 0 if everything went well, otherwise returns error code
+/// @return returns 0 if everything went well, otherwise returns 1
 int getNextToken(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool start(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool code(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool eol(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool type(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool definition(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool funcDefMid(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool functionParams(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool functionParamsN(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool functionParam(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool statements(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool statementsBlock(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool statement(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool statementOnLine(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool callOrAssign(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool varDec(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool varDecMid(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool varDef(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool returnExpression(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool arguments(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool argumentsN(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool argument(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool argWithName(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool argumentType(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool dataType(token *activeToken);
 
-/// @brief Function 
-/// @return returns true if is okok
+/// @brief Function of LL grammer
+/// @param activeToken pointer to token
+/// @return returns true if it was syntactically correct, otherwise returns false
 bool expression(token *activeToken);
 
 #endif
