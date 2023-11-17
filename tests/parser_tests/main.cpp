0911195c490d99a88a358647dfe8af2b9db2cdcf
Lukas Prokes <lukas.prokes03@gmail.com>
2023-11-17 20:24:46 +0100

    Parser tests working

--- tests/parser_tests/main.cpp
+++ tests/parser_tests/main.cpp
@@ -10,11 +10,18 @@
 #include "../../src/scanner.c"
 #include "../../src/scanner.h"
 #include "../../src/symtable.c"
-//#include "../../src/str.c"
+#include "../../src/symtable.h"
+#include "../../src/str.c"
+#include "../../src/str.h"
 #include "../../src/parser.c"
 #include "../../src/parser.h"
+#include "../../src/custom_data_types/list.c"
+#include "../../src/custom_data_types/list.h"
+#include "../../src/custom_data_types/hashtable.c"
+#include "../../src/custom_data_types/hashtable.h"
+#pragma once
 
-TEST(Parser, Test1){
+TEST(Parser, Test1_0){
     token *activeToken = (token *)malloc(sizeof(token));
     activeToken->value = (string*)malloc(sizeof(string));
     strInit(activeToken->value);
@@ -27,4 +34,19 @@ TEST(Parser, Test1){
     freopen("tests/parser_tests/test_inputs/test1_0", "r", stdin);
 
     EXPECT_EQ(parse(activeToken,symTable),0);
+}
+
+TEST(Parser, Test1_2){
+    token *activeToken = (token *)malloc(sizeof(token));
+    activeToken->value = (string*)malloc(sizeof(string));
+    strInit(activeToken->value);
+    activeToken->position = (positionInfo*)malloc(sizeof(positionInfo));
+    activeToken->position->charNumber = 0;
+    activeToken->position->lineNumber = 1;
+
+    symtable *symTable = symtableInit();
+
+    freopen("tests/parser_tests/test_inputs/test1_2", "r", stdin);
+
+    EXPECT_EQ(parse(activeToken,symTable),2);
 }
