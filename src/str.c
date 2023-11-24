fff8bf1b0165534e2dd51ee92555b891043dc23b
Lukas Prokes <lukas.prokes03@gmail.com>
2023-11-24 05:14:10 +0100

    RaiseError added to str.c

--- src/str.c
+++ src/str.c
@@ -17,7 +17,7 @@
 // funkce vytvori novy retezec
 int strInit(string *s) {
    if ((s->str = (char*) malloc(STR_LEN_INC)) == NULL)
-      return STR_ERROR;
+      raiseError(ERR_INTERNAL);
    s->str[0] = '\0';
    s->length = 0;
    s->allocSize = STR_LEN_INC;
@@ -42,7 +42,7 @@ int strAddChar(string *s1, char c) {
       // pamet nestaci, je potreba provest realokaci
       if ((s1->str = (char*) realloc(s1->str, s1->length + STR_LEN_INC)) == NULL) {   
            DEBUG_PRINTF("\n\nERRORNULL\n");
-         return STR_ERROR;
+         raiseError(ERR_INTERNAL);
       }
       s1->allocSize = s1->length + STR_LEN_INC;
    }
@@ -59,7 +59,7 @@ int strCopyString(string *s1, string *s2) {
    {
       // pamet nestaci, je potreba provest realokaci
       if ((s1->str = (char*) realloc(s1->str, newLength + 1)) == NULL) 
-         return STR_ERROR;
+         raiseError(ERR_INTERNAL);
       s1->allocSize = newLength + 1;
    }
    strcpy(s1->str, s2->str);
