2b80d9a3817e12e5401a4917ace49d450c57213d
Ondrej Vala <xvalao01@vutbr.cz>
2023-12-07 00:29:53 +0100

    Added comments

--- src/main.h
+++ src/main.h
@@ -8,6 +8,7 @@
 
 #pragma once
 
+/// @brief Shared structure for all parts of the program
 typedef struct{
     list *tokenQueue;
     generator *gen;
@@ -18,8 +19,16 @@ typedef struct{
     bool changeToDouble;
 } programState;
 
+/// @brief Initializes program state
+/// @return Initialized program state
 programState *programStateInit();
 
+/// @brief Cleans up program state
+/// @param state Initialized program state
 void programStateFree(programState *state);
 
+/// @brief Main function
+/// @param argc Number of arguments
+/// @param argv Arguments
+/// @return Exit code
 int main(int argc, char const *argv[]);
